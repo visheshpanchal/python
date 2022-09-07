cb7f99872410a5568a2c0ae8c27a7771d4e3cee5
Vishesh Panchal <visheshpanchal145@mail.com>
2022-09-07 19:07:36 +0530

    Tuple

---
+++ Data Structure/Tuple.py
@@ -0,0 +1,7 @@
+tp = (1, 2, 3, 4, 1, 2, 3, 5, 10)
+
+print("tp : ", tp.__repr__())
+
+print("Count Element in Tuple using tp.count(__value:any) : ", tp.count(1))
+
+print("Index of element using tp.index(element,start,stop) : ", tp.index(3))
